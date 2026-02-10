7c0aa545c2160c0e47a8acbccf37b45df0751703
Dev-ChickenHub <luonghongthai121011@gmail.com>
2026-02-10 21:33:53 +0700

    Update main.txt.lua

--- Game/Bloxfruit/SYNTRAXHub/main.txt.lua
+++ Game/Bloxfruit/SYNTRAXHub/main.txt.lua
@@ -1,3 +1,2 @@
 loadstring(game:HttpGet("https://luacrack.site/index.php/jgyvgfvyfctjh/raw/RynTraxHub/mainreal"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Testgameak/RYNtraxHub/refs/heads/main/attack%20fasst"))()
-loadstring(game:HttpGet("https://pastefy.app/zL0sYA8B/raw"))()
