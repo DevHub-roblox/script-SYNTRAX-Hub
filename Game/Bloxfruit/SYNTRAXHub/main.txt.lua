f90347a81efdc1cab514db2441f831620d502c6b
Dev-ChickenHub <luonghongthai121011@gmail.com>
2026-02-20 14:10:35 +0700

    Update script URL in main.txt.lua

--- Game/Bloxfruit/SYNTRAXHub/main.txt.lua
+++ Game/Bloxfruit/SYNTRAXHub/main.txt.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Testgameak/script.lua/refs/heads/main/12121112443"))()
+loadstring(game:HttpGet("https://pastefy.app/NsOwyV5Y/raw"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Testgameak/RYNtraxHub/refs/heads/main/attack%20fasst"))()
