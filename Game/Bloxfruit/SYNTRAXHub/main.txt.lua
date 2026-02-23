49d981f27d67b9ea63359ffa6ecf88ad382192d7
Dev-SynTraxHub <luonghongthai121011@gmail.com>
2026-02-23 18:44:41 +0700

    Add notify script to main.txt.lua

--- Game/Bloxfruit/SYNTRAXHub/main.txt.lua
+++ Game/Bloxfruit/SYNTRAXHub/main.txt.lua
@@ -1,2 +1,3 @@
 loadstring(game:HttpGet("https://pastefy.app/NsOwyV5Y/raw"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Testgameak/RYNtraxHub/refs/heads/main/attack%20fasst"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Teddyseetink/Haidepzai/refs/heads/main/notify"))()
