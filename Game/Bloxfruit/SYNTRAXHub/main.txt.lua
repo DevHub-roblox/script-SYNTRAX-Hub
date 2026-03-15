02bbb02a10fb0209975f5e9a6aa5bc8ffca35bc0
Dev-SynTraxHub <luonghongthai121011@gmail.com>
2026-03-15 21:05:38 +0700

    Fix loadstring calls in main.txt.lua

--- Game/Bloxfruit/SYNTRAXHub/main.txt.lua
+++ Game/Bloxfruit/SYNTRAXHub/main.txt.lua
@@ -1,2 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Testgameak/script.lua/refs/heads/main/mm2"))()
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Teddyseetink/Haidepzai/refs/heads/main/notify"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Testgameak/script.lua/refs/heads/main/mm2"))()
