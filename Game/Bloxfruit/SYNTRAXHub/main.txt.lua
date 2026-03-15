479572e8fbe14d5e54851a7a7b9bea5b1f539036
Dev-SynTraxHub <luonghongthai121011@gmail.com>
2026-03-15 21:12:50 +0700

    Update main.txt.lua

--- Game/Bloxfruit/SYNTRAXHub/main.txt.lua
+++ Game/Bloxfruit/SYNTRAXHub/main.txt.lua
@@ -1,3 +1,3 @@
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Teddyseetink/Haidepzai/refs/heads/main/notify"))()
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Testgameak/script.lua/refs/heads/main/mm2"))()
