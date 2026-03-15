8d86e35ca7a340189567da9a94d8a6bed6fe585b
Dev-SynTraxHub <luonghongthai121011@gmail.com>
2026-03-15 13:38:16 +0700

    Update main.txt.lua

--- Game/Bloxfruit/SYNTRAXHub/main.txt.lua
+++ Game/Bloxfruit/SYNTRAXHub/main.txt.lua
@@ -1,2 +1,4 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Testgameak/script.lua/refs/heads/main/SynTraxhubfix"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Testgameak/script.lua/refs/heads/main/attack"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Testgameak/RYNtraxHub/refs/heads/main/attack%20fasst"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Testgameak/RYNtraxHub/refs/heads/main/Fast%20attack.txt"))()
